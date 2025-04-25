c126adf719c871b96b6f99a80bef0ee8cc98b7f9
Cireneu Araujo <cireneuguilhermeteixeira>
2025-04-25 12:22:40 -0300

    Adding unit tests to connect with dynamo

--- poc/test/app_test.py
+++ poc/test/app_test.py
@@ -1,15 +1,47 @@
-from testcontainers.postgres import PostgresContainer
-import psycopg2
+from testcontainers.core.container import DockerContainer
+import boto3
+import time
 
-def test_postgres_connection():
-    with PostgresContainer("postgres:15") as postgres:
-        conn = psycopg2.connect(
-            host=postgres.get_container_host_ip(),
-            port=postgres.get_exposed_port(5432),
-            user=postgres.USER,
-            password=postgres.PASSWORD,
-            dbname=postgres.DB
+def test_dynamodb_com_localstack():
+    # Get LocalStack up and running with just DynamoDB
+    with DockerContainer("localstack/localstack:latest") \
+        .with_exposed_ports(4566) \
+        .with_env("SERVICES", "dynamodb") \
+        .with_env("EDGE_PORT", "4566") as localstack:
+
+        port = localstack.get_exposed_port(4566)
+        endpoint = f"http://localhost:{port}"
+
+        # Wait for LocalStack to initialize (may improve with healthcheck)
+        time.sleep(5)
+
+        # Create DynamoDB client pointing to the container
+        dynamodb = boto3.client(
+            "dynamodb",
+            endpoint_url=endpoint,
+            region_name="us-east-1",
+            aws_access_key_id="test",
+            aws_secret_access_key="test"
         )
-        cursor = conn.cursor()
-        cursor.execute("SELECT 1")
-        assert cursor.fetchone()[0] == 1
+
+        # Create  fake table
+        dynamodb.create_table(
+            TableName="TestTable",
+            KeySchema=[{"AttributeName": "id", "KeyType": "HASH"}],
+            AttributeDefinitions=[{"AttributeName": "id", "AttributeType": "S"}],
+            BillingMode="PAY_PER_REQUEST"
+        )
+
+        # Insert a item
+        dynamodb.put_item(
+            TableName="TestTable",
+            Item={"id": {"S": "123"}, "name": {"S": "Test"}}
+        )
+
+        # Get a item
+        resp = dynamodb.get_item(
+            TableName="TesteTabela",
+            Key={"id": {"S": "123"}}
+        )
+
+        assert resp["Item"]["name"]["S"] == "Test"
