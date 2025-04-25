ccf25913c12e729e81fe8190082518790f107a3f
Cireneu Araujo <cireneuguilhermeteixeira>
2025-04-25 12:23:02 -0300

    updating terraform with policies and roles

--- poc/terraform/main.tf
+++ poc/terraform/main.tf
@@ -5,35 +5,54 @@ provider "aws" {
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   endpoints {
-    iam = "http://localhost:4566"
+    dynamodb = "http://localhost:4566"
+    iam      = "http://localhost:4566"
+    sts      = "http://localhost:4566"
   }
 }
 
-resource "aws_iam_role" "db_access_role" {
-  name = "local-db-access"
+# DynamoDB Table
+resource "aws_dynamodb_table" "example_table" {
+  name         = "MyTable"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+# Simulating Role
+resource "aws_iam_role" "app_role" {
+  name = "MinhaAppRole"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Principal = {
-        Service = "ecs.amazonaws.com"
-      },
-      Effect = "Allow",
-    }]
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
   })
 }
 
-resource "aws_iam_policy" "db_policy" {
-  name        = "db-policy"
-  description = "Allow access to database"
+# DynamoDB Policy access 
+resource "aws_iam_policy" "dynamodb_policy" {
+  name        = "DynamoDBAccessPolicy"
 
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
         Action = [
-          "rds-db:connect"
+          "dynamodb:PutItem",
+          "dynamodb:GetItem",
+          "dynamodb:Scan"
         ],
         Effect   = "Allow",
         Resource = "*"
@@ -42,7 +61,8 @@ resource "aws_iam_policy" "db_policy" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "db_attach" {
-  role       = aws_iam_role.db_access_role.name
-  policy_arn = aws_iam_policy.db_policy.arn
+# Attach policy to role
+resource "aws_iam_role_policy_attachment" "attach" {
+  role       = aws_iam_role.app_role.name
+  policy_arn = aws_iam_policy.dynamodb_policy.arn
 }
